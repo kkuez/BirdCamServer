70a62f7188ec245f75fd6f2516dd349bee521cac
kkuez <Taumel1234>
2020-03-02 13:25:45 +0100

    Implemented converting raw File.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,62 +1,74 @@
 import org.apache.commons.io.FileUtils;
 
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.IOException;
-import java.io.InputStream;
+import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.time.LocalDateTime;
 import java.util.Properties;
 import java.util.Scanner;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 public class Main {
+
+    private static ExecutorService executorService = Executors.newFixedThreadPool(4);
 
     public static void main(String[] args) {
         System.out.println("Starting BirdcamServer");
 
         boolean exit = false;
+        File propertiesFile = new File("birdCamSetup.properties");
         Properties properties = new Properties();
-        File targetFolder = new File(properties.getProperty("videoTargetFolder"));
-
+        File targetFolder = null;
         try {
             properties.load(new FileInputStream(new File("birdCamSetup.properties")));
+            targetFolder = new File(properties.getProperty("videoTargetFolder"));
         } catch (IOException e) {
             e.printStackTrace();
             System.exit(1);
         }
 
-        System.out.println("Listening on 3333");
-        try(ServerSocket serverSocket = new ServerSocket(3333);
-            Socket client = serverSocket.accept();
-            InputStream clientInputStream = client.getInputStream();
-        ) {
-            while(!exit){
-                if(clientInputStream.available() > 0){
-                    String incomingDataString = new String(clientInputStream.readAllBytes());
-                    if(incomingDataString.startsWith("RECORDNOW")){
-                        System.out.println("Receiving Stream.");
+        while (!exit) {
+            System.out.println("Listening on 3333");
+            try (ServerSocket serverSocket = new ServerSocket(3333);
+                 Socket client = serverSocket.accept();
+                 InputStream clientInputStream = client.getInputStream();
+            ) {
+                System.out.println("Client connected: " + client.getRemoteSocketAddress().toString());
 
-                        File rawFolder = new File(".", "rawStreamFiles");
+                byte[] readByte = new byte[1];
+                String incomingDataString = "";
+                while (clientInputStream.available() > 0) {
+                    clientInputStream.read(readByte);
+                    incomingDataString += new String(readByte);
+                    if (incomingDataString.startsWith("RECORDNOW")) {
+                        System.out.println("Receiving Stream.");
+                        File rawFolder = new File("rawStreamFiles");
+                        if (!rawFolder.exists()) {
+                            rawFolder.mkdir();
+                        }
 
                         File rawFile = recordStream(rawFolder);
                         File convertedFile = convertStreamFile(rawFile);
                         File processedVidsFolder = new File(convertedFile.getParentFile().getParentFile(), "processed");
-                        if(!processedVidsFolder.exists()){
+                        if (!processedVidsFolder.exists()) {
                             processedVidsFolder.mkdir();
                         }
 
-                        File finalFile = new File(processedVidsFolder, convertedFile.getName());
+                        File finalFile = new File(targetFolder, convertedFile.getName());
                         FileUtils.copyFile(convertedFile, finalFile);
+                        convertedFile.delete();
                         System.out.println("File saved: " + finalFile.getAbsolutePath());
+                        incomingDataString = "";
                     }
                 }
+
+            } catch (IOException e) {
+                e.printStackTrace();
+                System.exit(2);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
             }
-        } catch (IOException e) {
-            e.printStackTrace();
-            System.exit(2);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
         }
     }
 
@@ -86,6 +98,7 @@ public class Main {
         processBuilderForCMD.redirectErrorStream(true);
         Process recProcess = processBuilderForCMD.start();
 
+        getExecutorService().submit(() -> {
             try(Scanner scanner = new Scanner(recProcess.getInputStream());
                 Scanner errScanner = new Scanner(recProcess.getErrorStream())){
                 while((scanner.hasNext() || errScanner.hasNext())) {
@@ -97,6 +110,7 @@ public class Main {
                     }
                 }
             }
+        });
 
         Thread.currentThread().sleep(10000);
         recProcess.destroyForcibly();
@@ -108,13 +122,13 @@ public class Main {
             processBuilderForCurl.command("cmd.exe",  "/k", "taskkill /IM curl.exe /F");
             processBuilderForCurl.redirectErrorStream(true);
             Process curlProcess = processBuilderForCurl.start();
-
+            getExecutorService().submit(() -> {
                 try (Scanner scanner = new Scanner(curlProcess.getInputStream())) {
                     while (scanner.hasNext() && curlProcess.isAlive()) {
                         System.out.println(scanner.next());
                     }
                 }
-
+            });
             Thread.currentThread().sleep(1000);
             curlProcess.destroyForcibly();
         }
@@ -122,8 +136,10 @@ public class Main {
 
         //Rename file so it will not be overwritten
         File rawFile = new File(currentOs == OS.Windows ? recFileLocation : "stream.raw");
-        FileUtils.copyFile(rawFile, new File(targetFolder, ("Bird" + LocalDateTime.now().toString() + ".mjpeg").replace(':', '_')));
-        return rawFile;
+        File finalFile = new File(targetFolder, ("Bird" + LocalDateTime.now().toString() + ".mjpeg").replace(':', '_'));
+        FileUtils.copyFile(rawFile, finalFile);
+        rawFile.delete();
+        return finalFile;
     }
 
     private static File convertStreamFile(File rawFile) throws IOException, InterruptedException {
@@ -145,8 +161,13 @@ public class Main {
                 System.out.println(errorStream);
             }
         }
+        File processedVid = new File(rawFile.getParent(), rawFile.getName() + ".mp4");
+        rawFile.delete();
+        return processedVid;
+    }
 
-        return new File(rawFile.getParent(), rawFile.getName() + ".mp4");
+    public static ExecutorService getExecutorService() {
+        return executorService;
     }
 
     private enum OS{
